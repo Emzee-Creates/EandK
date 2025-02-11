1c06d66eccee84291cc6e41067577b093f86bad0
Emzee-Creates <106532087+Emzee-Creates@users.noreply.github.com>
2025-02-11 11:09:49 +0100

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -1,4 +1,4 @@
-<?php
+n<?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -13,14 +13,14 @@ try {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';                       //gmail SMTP server set to send through
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'user@example.com';                     //SMTP username (your gmail account)
-    $mail->Password   = 'secret';                               //SMTP password (your gmail password or app password)
+    $mail->Username   = 'emmanuelemoruwa356@gmail.com';                     //SMTP username (your gmail account)
+    $mail->Password   = 'Youngmessi10!';                               //SMTP password (your gmail password or app password)
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = 465;
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');         //Set the sender of the message (your email address)
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient (your bae's email address)
+    $mail->setFrom('emmanuelemoruwa356@gmail.com', 'Mailer');         //Set the sender of the message (your email address)
+    $mail->addAddress('emmanuelemoruwa356@gmail.com', 'Joe User');     //Add a recipient (your bae's email address)
 
     //Content
     $mail->isHTML(true);
